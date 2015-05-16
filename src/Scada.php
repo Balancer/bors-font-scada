cef954d41a63d0e44a284635f23f29c0a299196c
Balancer <balancer@balancer.ru>
2015-05-16 15:25:04 +0300

    Code for load

--- src/Scada.php
+++ src/Scada.php
@@ -4,4 +4,10 @@ namespace B2\Font;
 
 class Scada
 {
+	static function baseUrl() { return '/_bors-assets/bors/font-scada'; }
+
+	static function load()
+	{
+		bors_use('pre:'.self::baseUrl().'/js/scada-local-storage-pre.js');
+	}
 }
